276621b610993123bf7adba3b7a4b03d8ee1d7b4
Bryan <kagingobrian2002@gmail.com>
2024-02-16 07:48:11 +0200

    updated message local storage on contact me form

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ document.getElementById("contact").addEventListener("submit", function (event) {
   // variable declaration and calling html id
   let fullname = document.getElementById("fullname").value;
   let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
   // data storage
   var userThings = {
     fullname: fullname,
     email: email,
+    message: message,
   };
   localStorage.setItem("userThings", JSON.stringify(userThings));
   window.location.href = "index.html";
