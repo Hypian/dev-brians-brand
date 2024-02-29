0f23e057e77a82eefbd3a49d5aab0c9d993c0da4
Bryan <kagingobrian2002@gmail.com>
2024-03-01 00:06:18 +0200

    new changes

--- server.ts
+++ server.ts
@@ -1,13 +1,10 @@
 import express, { Request, Response } from "express";
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import Joi from "joi";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import { swaggerSpec } from "./swagger";
-import swaggerUi from "swagger-ui-express";
-import path from "path";
 import bodyParser from "body-parser";
 
 dotenv.config();
@@ -17,7 +14,10 @@ const PORT = process.env.PORT || 3000;
 
 // Mongoose connection
 mongoose
-  .connect(process.env.MONGODB_URI!, {})
+  .connect("mongodb+srv://Hypian:Asad@123@atlascluster.z9xwgmj.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
@@ -27,41 +27,21 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-const likeSchema = new mongoose.Schema({
-  userEmail: { type: String, required: true },
-});
-
-const dislikeSchema = new mongoose.Schema({
-  userEmail: { type: String, required: true },
-});
-
-const commentSchema = new mongoose.Schema({
-  userEmail: { type: String, required: true },
-  comment: { type: String, required: true },
-});
-
-const contactSchema = new mongoose.Schema({
-  fullname: { type: String, required: true },
-  email: { type: String, required: true },
-  message: { type: String, required: true },
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  imageUrl: { type: String, required: true },
 });
 
 // Define models
 const User = mongoose.model("User", userSchema);
-const Like = mongoose.model("Like", likeSchema);
-const Dislike = mongoose.model("Dislike", dislikeSchema);
-const Comment = mongoose.model("Comment", commentSchema);
-const Contact = mongoose.model("Contact", contactSchema);
+const Blog = mongoose.model("Blog", blogSchema);
 
 // Middleware
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static("public"));
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-
-// Serve Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Signup endpoint
 app.post("/api/signup", async (req, res) => {
@@ -116,130 +96,32 @@ interface RequestWithUser extends Request {
   user: { userId: string };
 }
 
-// Like endpoint
-app.post("/api/like", async (req: any, res: any) => {
-  // Validate request body
-  const { error } = likeValidationSchema.validate(req.body);
-  if (error) return res.status(400).json({ error: error.details[0].message });
+// Blog post endpoint
+app.post(
+  "/api/blog",
+  authenticateToken,
+  async (req: RequestWithUser, res: Response) => {
+    // Validate request body
+    const { error } = blogSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
 
-  // Save like to database
-  const like = new Like({
-    userEmail: req.user.userId,
-  });
-
-  try {
-    await like.save();
-    res.status(200).json({ message: "Like saved successfully" });
-  } catch (error) {
-    console.error("Error saving like:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Define a Joi schema for like validation
-const likeValidationSchema = Joi.object({
-  userEmail: Joi.string().email().required(),
-});
-
-// Dislike endpoint
-app.post("/api/dislike", async (req: any, res: any) => {
-  // Validate request body
-  const { error } = dislikeValidationSchema.validate(req.body);
-  if (error) return res.status(400).json({ error: error.details[0].message });
-
-  // Save dislike to database
-  const dislike = new Dislike({
-    userEmail: req.user.userId, // Assuming you store user ID in req.user
-  });
-
-  try {
-    await dislike.save();
-    res.status(200).json({ message: "Dislike saved successfully" });
-  } catch (error) {
-    console.error("Error saving dislike:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Define a Joi schema for dislike validation
-const dislikeValidationSchema = Joi.object({
-  userEmail: Joi.string().email().required(),
-});
-
-// Comment endpoint
-app.post("/api/comment", async (req: any, res: any) => {
-  // Validate request body
-  const { error } = commentValidationSchema.validate(req.body);
-  if (error) return res.status(400).json({ error: error.details[0].message });
-
-  // Save comment to database
-  const comment = new Comment({
-    userEmail: req.user.userId,
-    comment: req.body.comment,
-  });
-
-  try {
-    await comment.save();
-    res.status(200).json({ message: "Comment saved successfully" });
-  } catch (error) {
-    console.error("Error saving comment:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Define a Joi schema for comment validation
-const commentValidationSchema = Joi.object({
-  userEmail: Joi.string().email().required(),
-  comment: Joi.string().required(),
-});
-
-// Contact form endpoint
-app.post("/api/contact", async (req, res) => {
-  // Validate request body
-  const { error } = contactValidationSchema.validate(req.body);
-  if (error) return res.status(400).json({ error: error.details[0].message });
-
-  // Save contact form message to database
-  const contact = new Contact({
-    fullname: req.body.fullname,
-    email: req.body.email,
-    message: req.body.message,
-  });
-
-  try {
-    await contact.save();
-    res.status(200).json({ message: "Message sent successfully" });
-  } catch (error) {
-    console.error("Error saving contact form message:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Define a Joi schema for contact form validation
-const contactValidationSchema = Joi.object({
-  fullname: Joi.string().required(),
-  email: Joi.string().email().required(),
-  message: Joi.string().required(),
-});
-
-// Save contact form message to database
-
-// Assuming this is within an Express route handler
-app.post("/api/contact", async (req: Request, res: Response) => {
-  try {
-    const contact = new Contact({
-      fullname: req.body.fullname,
-      email: req.body.email,
-      message: req.body.message,
+    // Create new blog post
+    const blog = new Blog({
+      title: req.body.title,
+      content: req.body.content,
+      imageUrl: req.body.imageUrl,
     });
 
-    await contact.save();
-    res.status(200).json({ message: "Message sent successfully" });
-  } catch (error) {
-    console.error("Error saving contact form message:", error);
-    res.status(500).json({ error: "Internal server error" });
+    try {
+      // Save blog post to database
+      await blog.save();
+      res.status(201).json({ message: "Blog post created successfully" });
+    } catch (error) {
+      console.error("Error creating blog post:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
-});
+);
 
 // Middleware function to authenticate token
 function authenticateToken(
@@ -258,12 +140,7 @@ function authenticateToken(
   });
 }
 
-// Define RequestWithUser interface
-interface RequestWithUser extends Request {
-  user: { userId: string };
-}
-
-// Signup schema validation function
+// Define Joi schema for user signup validation
 function userSignupSchema(body: any) {
   const schema = Joi.object({
     email: Joi.string().email().required(),
@@ -272,7 +149,7 @@ function userSignupSchema(body: any) {
   return schema.validate(body);
 }
 
-// Login schema validation function
+// Define Joi schema for user login validation
 function userLoginSchema(body: any) {
   const schema = Joi.object({
     email: Joi.string().email().required(),
@@ -281,17 +158,17 @@ function userLoginSchema(body: any) {
   return schema.validate(body);
 }
 
-// Contact form schema validation function
-function contactFormSchema(body: any) {
+// Define Joi schema for blog post validation
+function blogSchemaValidation(body: any) {
   const schema = Joi.object({
-    fullname: Joi.string().required(),
-    email: Joi.string().email().required(),
-    message: Joi.string().required(),
+    title: Joi.string().required(),
+    content: Joi.string().required(),
+    imageUrl: Joi.string().uri().required(),
   });
   return schema.validate(body);
 }
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${5500}`);
+  console.log(`Server is running on port ${PORT}`);
 });
